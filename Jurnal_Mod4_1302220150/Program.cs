ce88482761aaac439ee43e7599819a62a0f5e3b4
Haikal Risnandar <115600487+Heris56@users.noreply.github.com>
2024-03-12 18:13:37 -0700

    Add files via upload
    menambahkan class Posisi karakter

--- Jurnal_Mod4_1302220150/Program.cs
+++ Jurnal_Mod4_1302220150/Program.cs
@@ -1,7 +1,22 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+//Command Kode Buah
 KodeBuah buah = new KodeBuah();
 buah.getKodeBuah("Anggur");
+buah.getKodeBuah("abc");
+
+//Command Posisi Karakter
+PosisiKarakter kar = new PosisiKarakter();
+Console.WriteLine("Posisi StandBy");
+Console.WriteLine("Masukan Command: ");
+String Command = Console.ReadLine();
+while (Command != "0")
+{
+    kar.bergerak(Command);
+    
+    Console.WriteLine("Masukan Command: ");
+    Command = Console.ReadLine();
+}
+
 public class KodeBuah
 {
     String[] NamaBuah = {"Apel", "Aprikot", "Alpukat","Pisang", "Paprika", "Blackberry", "ceri", "Kelapa"
@@ -32,5 +47,73 @@ public class KodeBuah
 
 public class PosisiKarakter
 {
+    enum Karakter { Jongkok, Berdiri, Tengkurap, Terbang };
+    Karakter karakter = Karakter.Berdiri;
 
+    //String[] moveset = { "Jongkok", "Berdiri", "Tengkurap", "Terbang" };
+
+    public void bergerak(String gerakan)
+    {
+        //Console.WriteLine("Karakter " + moveset[(int)karakter]);
+        switch (karakter)
+        {
+            case Karakter.Jongkok:
+                if (gerakan == "TombolS")
+                {
+                    karakter = Karakter.Tengkurap;
+                    Console.WriteLine("Posisi Istirahat");
+                } else if (gerakan == "TombolW")
+                {
+                    karakter = Karakter.Berdiri;
+                    Console.WriteLine("Posisi StandBy");
+                }
+                else
+                {
+                    karakter = Karakter.Jongkok;
+                }
+                break;
+            case Karakter.Berdiri:
+                if (gerakan == "TombolS")
+                {
+                    karakter = Karakter.Jongkok;
+                }
+                else if (gerakan == "TombolW")
+                {
+                    karakter = Karakter.Terbang;
+                }
+                else
+                {
+                    karakter = Karakter.Berdiri;
+                    Console.WriteLine("Posisi StandBy");
+                }
+                break;
+            case Karakter.Terbang:
+                if (gerakan == "TombolS")
+                {
+                    karakter = Karakter.Berdiri;
+                    Console.WriteLine("Posisi StandBy");
+                }
+                else if (gerakan == "TombolX")
+                {
+                    karakter = Karakter.Jongkok;
+                }
+                else
+                {
+                    karakter = Karakter.Terbang;
+                }
+                break;
+            case Karakter.Tengkurap:
+                if (gerakan == "TombolW")
+                {
+                    karakter = Karakter.Jongkok;
+                }
+                else
+                {
+                    karakter = Karakter.Tengkurap;
+                    Console.WriteLine("Posisi Istirahat");
+                }
+                break;
+        }
+        
+    }
 }
